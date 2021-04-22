46a9ea39b86a3d1bdf8fd9d7df98dac4a02205d1
Tony <gma201011@hotmail.com>
2021-04-22 17:34:32 +0800

    hw2-1

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,5 +1,7 @@
-function reverse(str) {
-  
-}
-
-reverse('hello');
+function reverse(str){
+    var result = ''
+    for(i = str.length - 1; i >= 0; i--){
+      result += str[i]
+    }
+    console.log(result)
+  }
