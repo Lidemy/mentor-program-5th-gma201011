8bb97ce4e3fabb824a5b4b4a6c7412cecc7a52b5
Tony <gma201011@hotmail.com>
2021-05-09 11:09:07 +0800

    week4_ver2

--- homeworks/week4/hw1.js
+++ homeworks/week4/hw1.js
@@ -4,15 +4,18 @@ request.get(
   'https://lidemy-book-store.herokuapp.com/books?_limit=10',
   (error, response, body) => {
     if (error) {
-      return console.log('讀取失敗', error)
+      console.log('讀取失敗', error)
+      return
     }
     let data
     try {
       data = JSON.parse(body)
-    } catch (e) {
-      console.log(e)
+    } catch (error) {
+      return console.log(error)
     }
-    for (let i = 0; i < data.length; i++) {
-      console.log(`${data[i].id} ${data[i].name}`)
+    if (response.statusCode >= 200 && response.statusCode < 300) {
+      for (let i = 0; i < data.length; i++) {
+        console.log(`${data[i].id} ${data[i].name}`)
+      }
     }
   })
